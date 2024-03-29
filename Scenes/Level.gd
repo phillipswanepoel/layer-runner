1220333d56c088123e4171dbf6ee6c70f212bcb3
phillipswanepoel <phillipswanepoel@gmail.com>
2024-03-29 13:06:57 +0200

    stuff

--- Scenes/Level.gd
+++ Scenes/Level.gd
@@ -11,16 +11,16 @@ func _ready():
 	player.change_player_layer.connect(_on_player_layer_change)
 	
 @onready var layers = [$FG, $MG, $BG]
-func _on_player_layer_change(new_layer: int):	
-	print("SIGNAL CALLED")
+func _on_player_layer_change(new_layer: int):		
 	player.reparent(layers[new_layer])
 
 var speed : float = 60
 var progress : float = 0.0
-var fg_speed_factor : float = 1.0
+var fg_speed_factor : float = 0.8
 var mg_speed_factor : float = 0.6
 var bg_speed_factor : float = 0.3
 
+var cum_delta : float = 0
 func _physics_process(delta):
 	var distance = speed * delta
 	var path_followers = get_tree().get_nodes_in_group("path_followers")
@@ -37,10 +37,17 @@ func _physics_process(delta):
 		pf.progress += mod_distance
 		if pf.progress_ratio > 0.95:
 			pf.queue_free()
+			
+	cum_delta += delta
+	if cum_delta > 1:
+		cum_delta = 0
+		speed += 1.5
 		
 @export var fg_buildings : Array[PackedScene] = []
 @onready var fg_timer = $FG/FGTimer
 @onready var fg_path = $FG/Path2D
+var rand_floor = 0.75
+var rand_ceiling = 0.9
 func _on_fg_timer_timeout() -> void:		
 	#spawn a random fg building, attach to its movement path
 	var chosen_building = fg_buildings.pick_random().instantiate()	
@@ -52,7 +59,7 @@ func _on_fg_timer_timeout() -> void:
 	path_follow.add_child(chosen_building)
 	
 	#start timer with some random interval
-	var random_wait : float = rng.randf_range(0.7, 1.5)*(1/fg_speed_factor)
+	var random_wait : float = rng.randf_range(rand_floor, rand_ceiling)*(1/fg_speed_factor)
 	fg_timer.start(random_wait)
 	
 @export var mg_buildings : Array[PackedScene] = []
@@ -74,7 +81,7 @@ func _on_mg_timer_timeout() -> void:
 	path_follow.add_child(chosen_building)
 	
 	#start timer with some random interval
-	var random_wait : float = rng.randf_range(0.7, 2.0)*(1/mg_speed_factor)
+	var random_wait : float = rng.randf_range(rand_floor, rand_ceiling)*(1/mg_speed_factor)
 	mg_timer.start(random_wait)
 	
 @export var bg_buildings : Array[PackedScene] = []
@@ -93,6 +100,16 @@ func _on_bg_timer_timeout() -> void:
 	path_follow.add_child(chosen_building)
 	
 	#start timer with some random interval
-	var random_wait : float = rng.randf_range(0.7, 2.0)*(1/bg_speed_factor)
+	var random_wait : float = rng.randf_range(rand_floor-0.1, rand_ceiling-0.1)*(1/bg_speed_factor)
 	bg_timer.start(random_wait)
 
+
+func _on_building_gaps_timeout() -> void:
+	rand_ceiling += 0.1
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_death_zone_body_entered(body: Node2D) -> void:
+	$Control/Button.disabled = false
+	$Control/Button.visible = true
